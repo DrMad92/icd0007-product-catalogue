f480234e9afacdf975cb912aa3bd8cce2af8ad1e
mumadr <mumadr@ttu.ee>
2019-04-20 18:55:32 +0300

    Working add, delete, edit category buttons

--- js/category.js
+++ js/category.js
@@ -6,27 +6,32 @@ $(document).ready(function() {
         $("#" + id).show();
         $("#" + id).html('<object type="text/html" data="src/products.php?category=' + id + '" ></object>');
     });
-    $("#addForm-add-button").click(function(){
-        $("#addForm").show();
+    $("[id$=Form-open-button]").click(function(){
+        var id = $(this).attr('id').replace("-open-button","");
+        $("#" + id).show();
     });
-    $("#addForm-close-button").click(function(){
-        $("#addForm").hide();
+    $("[id$=Form-close-button]").click(function(){
+        var id = $(this).attr('id').replace("-close-button","");
+        $("#" + id).hide();
     });
-    $("#addForm-submit").submit(function(e) {
+    $("[type=submit]").click(function(e) {
         e.preventDefault();
-        var form = $(this);
+        var buttonName = $(this).attr('name');
+        var form = $(this).parents('form:first');
         var url = form.attr('action');
         var method = form.attr('method');
     
         $.ajax({
                method: method,
                url: url,
-               data: form.serialize(),
+               data: form.serialize() + "&" + buttonName + "=",
                error: function(err){
+                    console.log(err);
                     alert(err.responseJSON.message);
                },
                success: function(resp)
                {
+                    console.log(resp);
                     alert(resp.message);
                     window.location.reload();
                }
--- src/action_category.php
+++ src/action_category.php
@@ -3,26 +3,66 @@
 require_once '../vendor/autoload.php';
 require_once '../propel/config.php';
 
-if($_SERVER['REQUEST_METHOD'] == "POST")
+if($_SERVER['REQUEST_METHOD'] === "POST")
 {
-    $category = $_POST['name'];
-    unset($_POST);
-    $category = preg_replace("/[^a-zA-Z0-9]+/", "", $category); // Leave only alpha numeric
-    
-    $query = CategoryQuery::create()->findOneByName($category);
-    if ($query) {
-        header('HTTP/1.1 490 Category exists');
+    if (isset($_POST['add'])) {
+        $category = $_POST['name'];
+        unset($_POST);
+        $category = preg_replace("/[^a-zA-Z0-9]+/", "", $category); // Leave only alpha numeric
+        
+        if ($category == ""){
+            header('HTTP/1.1 491 Empty category name');
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode(array('message' => 'Empty category name', 'code' => 491)));
+        }
+        $query = CategoryQuery::create()->findOneByName($category);
+        if ($query) {
+            header('HTTP/1.1 490 Category exists');
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode(array('message' => 'Category exists', 'code' => 490)));
+        }
+
+        $cat = new Category();
+        $cat -> setName($category);
+        $cat -> save();
+
+        header('HTTP/1.1 200 OK');
         header('Content-Type: application/json; charset=UTF-8');
-        die(json_encode(array('message' => 'Category exists', 'code' => 490)));
+        die(json_encode(array('message' => 'Success', 'code' => 200)));    
+    
+    } elseif(isset($_POST['save'])) {
+        if (empty($_POST['name'])){
+            header('HTTP/1.1 492 Empty name');
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode(array('message' => 'Empty rename', 'code' => 492)));
+        }
+        $category = $_POST['category'];
+        $newName = $_POST['name'];
+        unset($_POST); 
+        $newName = preg_replace("/[^a-zA-Z0-9]+/", "", $newName); // Leave only alpha numeric
+
+        $query = CategoryQuery::create()
+            ->filterByName($category)
+            ->update(array('Name' => $newName));
+        
+        header('HTTP/1.1 200 OK');
+        header('Content-Type: application/json; charset=UTF-8');
+        die(json_encode(array('message' => 'Renamed', 'code' => 200)));
+
+    } elseif (isset($_POST['delete'])){
+        $category = $_POST['category'];
+        unset($_POST);
+
+        $query = CategoryQuery::create()->findOneByName($category);
+        $productList = ProductQuery::create() -> filterByCategory($query) -> delete();;
+        $query->delete();
+        
+        header('HTTP/1.1 200 OK');
+        header('Content-Type: application/json; charset=UTF-8');
+        die(json_encode(array('message' => 'Removed', 'code' => 200)));
+
     }
-
-    $cat = new Category();
-    $cat -> setName($category);
-    $cat -> save();
-
-    header('HTTP/1.1 200 OK');
-    header('Content-Type: application/json; charset=UTF-8');
-    die(json_encode(array('message' => 'Success', 'code' => 200)));
+    
 }
 
 ?>
