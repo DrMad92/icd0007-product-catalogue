ad9874b9e31b9a7a876e9f2d00c198b5258e6241
mumadr <mumadr@ttu.ee>
2019-04-23 00:00:40 +0300

    added PID exists status

--- lib/Response.php
+++ lib/Response.php
@@ -7,13 +7,15 @@ class Response extends BaseResponse
     const HTTP_CATEGORY_EXISTS = 490;
     const HTTP_EMPTY_CATEGORY_NAME = 491;
     const HTTP_EMPTY_NAME = 492;
-    const HTTP_EMPTY_PID = 493;
+    const HTTP_PID_EXISTS = 493;
+    const HTTP_EMPTY_PID = 494;
 
     public static $statusTexts = [
         490 => 'Category Exists',
         491 => 'Empty Category Name',
         492 => 'Empty Name',
-        493 => 'Empty PID'
+        493 => 'PID Exists',
+        494 => 'Empty PID'
     ];
 
     public function __construct($content = '', $status = 200, $headers = array())
